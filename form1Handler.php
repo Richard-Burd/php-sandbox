6287678469d996a7c2daeb86fcf991992b511573
Richard-Burd <qalbalassadalasfour@gmail.com>
2020-07-02 15:26:50 -0700

    PHP & HTML on seperate scripts

---
+++ form1Handler.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>This will return the name</title>
+  </head>
+  <body>
+    <?php
+    // user input w/Form
+    $theName = $_GET["myName"];
+    echo "<h2>The name you entered was $theName </h2>";
+    ?>
+  </body>
+</html>
