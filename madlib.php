10ba8fc96ff9ce72aac4eaa8b3a4787320e0bb1b
Richard-Burd <qalbalassadalasfour@gmail.com>
2020-07-02 15:28:31 -0700

    GET method used

---
+++ madlib.php
@@ -0,0 +1,15 @@
+<form action="madlib.php" method="get">
+  Color: <input type="text" name="color"> <br>
+  Plural Noun: <input type="text" name="pluralNoun"> <br>
+  Celebrity: <input type="text" name="celebrity"> <br>
+  <input type="submit">
+</form>
+<br>
+<?php
+  $color = $_GET["color"];
+  $pluralNoun = $_GET["pluralNoun"];
+  $celebrity = $_GET["celebrity"];
+  echo "Roses are $color <br>";
+  echo "$pluralNoun are blue <br>";
+  echo "I love $celebrity <br>";
+?>
