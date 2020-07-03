521ea2cdc4c0b1865d40cb3970da59d6567b673e
Richard-Burd <qalbalassadalasfour@gmail.com>
2020-07-02 17:59:12 -0700

    include variable values

---
+++ include2.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>How to Include other Files</title>
+  </head>
+  <body>
+    <?php
+      $title = "My first post";
+      $author = "Bob";
+      $wordcCount = 400;
+      include "article-header.php"
+    ?>
+  </body>
+</html>
