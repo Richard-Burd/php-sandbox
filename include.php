9aab71ba2298e6c2ee69bab18cf62c9fdca917d4
Richard-Burd <qalbalassadalasfour@gmail.com>
2020-07-02 17:40:03 -0700

    including modular components

---
+++ include.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>How to Include other Files</title>
+  </head>
+  <body>
+    <?php include "header.html" ?>
+      <p>Website main body</p>
+    <?php include "footer.html" ?>
+  </body>
+</html>
